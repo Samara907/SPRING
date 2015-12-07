aacd52b1842ebe08f402b1033cc3904633ecbf0a
samara907 <mabin.kjy@gmail.com>
2015-12-07 12:15:00 +0900

    spring download

--- WebContent/test/filedownload.jsp
+++ WebContent/test/filedownload.jsp
@@ -4,12 +4,9 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>location = /test/test.jsp</title>
+<title>location = /test/filedownload.jsp</title>
 </head>
 <body>
-	<form action="/spring/test.do" enctype="multipart/form-data" method="post">
-		upload<input type="file" name="upload"/><br />
-			  <input type="submit" value="업로드"/>
-	</form>
+	FILE DOWNLOAD SUCCESS
 </body>
 </html>
--- src/test/spring/bean/HelloBean.java
+++ src/test/spring/bean/HelloBean.java
@@ -4,18 +4,20 @@ import java.io.File;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloBean {
-
-	@RequestMapping("/test.do")
-	public String test(MultipartHttpServletRequest request) throws Exception {
-		MultipartFile file = request.getFile("upload");
-		String name = file.getOriginalFilename();
-		File sf = new File("e://save//" + name);
-		file.transferTo(sf);
-		return "/test/fileupload.jsp";
+	
+	/**
+	 * @return
+	 * @throws Exception
+	 * new ModelAndView(bean name, parameter name, down file)
+	 */
+	@RequestMapping("/download.do")
+	public ModelAndView down() throws Exception {
+		File down = new File("e://save//temp.jpg");
+		ModelAndView mv = new ModelAndView("download", "downloadFile", down);
+		return mv;
 	}
 }
